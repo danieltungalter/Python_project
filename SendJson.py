698387d203ff4a316af9d5ffd0977903662132cc
danieltungalter <36910582+danieltungalter@users.noreply.github.com>
2018-11-16 18:19:58 +0800

    Update SendJson.py

--- SendJson.py
+++ SendJson.py
@@ -2,10 +2,10 @@ import requests
 import json
 
 url = 'http://career.wemine.hk/cv-submit'
-messages = {"name": "Tung Chi Hung",
-            "email": "danieltunghk@gmail.com",
-            "position": "Web Developer",
-            "cv_url": "https://drive.google.com/open?id=1Qy5RTqvvARG_MiejFgGeYKd_nE1qNv2S"}
+messages = {"name": " ",
+            "email": " ",
+            "position": " ",
+            "cv_url": " "}
 headers = {'content-type': 'application/json'}
 
 response = requests.post(url, data=json.dumps(messages), headers=headers)
